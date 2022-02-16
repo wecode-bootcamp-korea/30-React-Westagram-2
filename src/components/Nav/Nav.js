cfbf8c55b5b87515a1631c792fc01acaa8d87f58
gunhee-jeong <kyle4619@naver.com>
2022-02-16 16:05:15 +0900

    Completion: Initialization

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,35 @@
+import "./Nav.scss";
+
+function Nav() {
+  return (
+    <nav className="nav">
+      <div className="inner">
+        <div className="nav_logo">
+          <a href="#">
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a href="#">
+            <h1>Instagram</h1>
+          </a>
+        </div>
+        <div className="nav_search">
+          <input type="text" placeholder="검색" />
+          <i className="fas fa-search"></i>
+        </div>
+        <div className="nav_icon">
+          <a href="#">
+            <i className="far fa-compass"></i>
+          </a>
+          <a href="#">
+            <i className="far fa-heart"></i>
+          </a>
+          <a href="#">
+            <i className="far fa-user"></i>
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
