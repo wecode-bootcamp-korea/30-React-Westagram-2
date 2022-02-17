104280eb6523ac44a20d945dd6c2e8a39af47e2c
Seonghun Kang <gceohoony@gmail.com>
2022-02-17 21:27:08 +0900

    Add: Login, Main completion

--- src/pages/sung/LoginSung/LoginSung.js
+++ src/pages/sung/LoginSung/LoginSung.js
@@ -2,7 +2,29 @@ import React from 'react';
 import './LoginSung.scss';
 
 function LoginSung() {
-  return;
+  return (
+    <article className="container_login">
+      <div className="mainBox_login">
+        <h1 className="login_logo">westagram</h1>
+        <div className="login_box">
+          <input
+            type="text"
+            placeholder="Phone number, username, or email"
+            id="id_input"
+            className="elementOfLogin"
+          />
+          <input
+            type="password"
+            id="password_input"
+            placeholder="Password"
+            className="elementOfLogin"
+          />
+          <button className="loginbtn deactivate_btn">Log in</button>
+        </div>
+        <button id="forgot_password">Forgot password?</button>
+      </div>
+    </article>
+  );
 }
 
 export default LoginSung;
