70c452b20f4d019f0c4aa1cdfd01bd071356796f
gunhee-jeong <kyle4619@naver.com>
2022-02-17 17:32:17 +0900

    Add: 작업하던 파일 이동 완료

--- src/pages/kyle/LoginKyle/LoginKyle.js
+++ src/pages/kyle/LoginKyle/LoginKyle.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './LoginKyle.scss';
 
 function LoginKyle() {
-  return;
+  const navigate = useNavigate();
+
+  const goToMain = () => {
+    navigate('/Main-Kyle');
+  };
+
+  return (
+    <main className="LoginContainer">
+      <div className="loginInner">
+        <h1>Westagram</h1>
+        <form className="loginForm" action="">
+          <input
+            id="id"
+            type="text"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+          />
+          <input id="pw" type="password" placeholder="비밀번호" />
+          {/* disabled */}
+          <button id="loginBtn" type="button" onClick={goToMain}>
+            로그인
+          </button>
+        </form>
+        <span className="passwordContainer">
+          <a href="#">비밀번호를 잊으셨나요?</a>
+        </span>
+      </div>
+    </main>
+  );
 }
 
 export default LoginKyle;
--- src/pages/kyle/MainKyle/MainKyle.js
+++ src/pages/kyle/MainKyle/MainKyle.js
@@ -1,8 +1,175 @@
 import React from 'react';
+import Nav from '../../../components/Nav/Nav';
 import './MainKyle.scss';
 
-function MainKyle() {
-  return;
-}
+const MainKyle = () => {
+  return (
+    <div className="Main">
+      <Nav />
+      <main>
+        <div className="feeds">
+          <article>
+            <div className="feedsHeader">
+              <div className="user">
+                <img
+                  src="https://gunhee-jeong.github.io/assets/images/blogMe1.jpeg"
+                  alt="프로필 사진"
+                  className="profileImg"
+                />
+                <span>gunhee_jeong</span>
+              </div>
+              <button></button>
+            </div>
+            <img
+              src="images/kyle/jeju.jpeg"
+              alt="제주도 하늘"
+              className="feedPhoto"
+            />
+            <div className="commentWrapper">
+              <div className="menuNav">
+                <div className="conversation">
+                  <button></button>
+                  <button></button>
+                  <button></button>
+                </div>
+                <div className="save">
+                  <button></button>
+                </div>
+              </div>
+              <div>좋아요 574개</div>
+              <div className="commentlike">
+                <div className="likeSession"></div>
+                <p>
+                  <span className="name">gunhee</span>님 외 xx명이 좋아합니다
+                </p>
+              </div>
+              <ul className="commentLists">
+                <li>
+                  <span className="name">gunhee</span>
+                  <span>제주도 푸른밤</span>
+                  <span className="delete">x</span>
+                </li>
+              </ul>
+            </div>
+            <div className="comment">
+              <input id="commentInput" type="text" placeholder="댓글 달기" />
+              <button id="submit">게시</button>
+            </div>
+          </article>
+        </div>
+        <aside className="main-right">
+          <div className="recommendation">
+            <div>스토리</div>
+            <ul>
+              <li>
+                <div className="image">
+                  <img
+                    src="https://gunhee-jeong.github.io/assets/images/blogMe1.jpeg"
+                    alt="프로필 사진"
+                    className="avatarImage"
+                  />
+                </div>
+                <div className="info">
+                  <div className="recoId">gunhee</div>
+                  <div>gunhee님이 팔로우합니다.</div>
+                </div>
+                <button></button>
+              </li>
+              <li>
+                <div className="image">
+                  <img
+                    src="https://gunhee-jeong.github.io/assets/images/blogMe1.jpeg"
+                    alt="프로필 사진"
+                    className="avatarImage"
+                  />
+                </div>
+                <div className="info">
+                  <div className="recoId">gunhee</div>
+                  <div>gunhee님이 팔로우합니다.</div>
+                </div>
+                <button></button>
+              </li>
+              <li>
+                <div className="image">
+                  <img
+                    src="https://gunhee-jeong.github.io/assets/images/blogMe1.jpeg"
+                    alt="프로필 사진"
+                    className="avatarImage"
+                  />
+                </div>
+                <div className="info">
+                  <div className="recoId">gunhee</div>
+                  <div>gunhee님이 팔로우합니다.</div>
+                </div>
+                <button></button>
+              </li>
+              <li>
+                <div className="image">
+                  <img
+                    src="https://gunhee-jeong.github.io/assets/images/blogMe1.jpeg"
+                    alt="프로필 사진"
+                    className="avatarImage"
+                  />
+                </div>
+                <div className="info">
+                  <div className="recoId">gunhee</div>
+                  <div>gunhee님이 팔로우합니다.</div>
+                </div>
+                <button></button>
+              </li>
+              <li>
+                <div className="image">
+                  <img
+                    src="https://gunhee-jeong.github.io/assets/images/blogMe1.jpeg"
+                    alt="프로필 사진"
+                    className="avatarImage"
+                  />
+                </div>
+                <div className="info">
+                  <div className="recoId">gunhee</div>
+                  <div>gunhee님이 팔로우합니다.</div>
+                </div>
+                <button></button>
+              </li>
+              <li>
+                <div className="image">
+                  <img
+                    src="https://gunhee-jeong.github.io/assets/images/blogMe1.jpeg"
+                    alt="프로필 사진"
+                    className="avatarImage"
+                  />
+                </div>
+                <div className="info">
+                  <div className="recoId">gunhee</div>
+                  <div>gunhee님이 팔로우합니다.</div>
+                </div>
+                <button></button>
+              </li>
+              <li>
+                <div className="image">
+                  <img
+                    src="https://gunhee-jeong.github.io/assets/images/blogMe1.jpeg"
+                    alt="프로필 사진"
+                    className="avatarImage"
+                  />
+                </div>
+                <div className="info">
+                  <div className="recoId">gunhee</div>
+                  <div>gunhee님이 팔로우합니다.</div>
+                </div>
+                <button></button>
+              </li>
+            </ul>
+            <div className="Profile">
+              <div className="avatar"></div>
+              <div></div>
+            </div>
+            <div></div>
+          </div>
+        </aside>
+      </main>
+    </div>
+  );
+};
 
 export default MainKyle;
